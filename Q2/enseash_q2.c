83dcbfd9c6e3dcee1cf8cc022f07eb5fd22579c7
william-nicolussi <188550990+william-nicolussi@users.noreply.github.com>
2024-12-04 21:36:10 +0100

    enseash_q2.c with writeOrExit function

--- Q2/enseash_q2.c
+++ Q2/enseash_q2.c
@@ -10,6 +10,8 @@
 #define PROMPT_MESSAGE "enseash % "
 #define EXIT_MESSAGE "Bye bye...\n"
 #define COMMAND_NOT_FOUND_MESSAGE "Command not found.\n"
+#define READ_ERROR_MESSAGE "\nError: Unable to read input.\n"
+#define WRITE_ERROR_MESSAGE "\nError: Unable to write.\n"
 
 #define FORTUNE_MESSAGE "Today is what happened to yesterday.\n"
 #define FORTUNE_STRING_INPUT "fortune"
@@ -17,6 +19,53 @@
 #define FORK_FAILED_MESSAGE "fork failed"
 #define COMMAND_FAILED_MESSAGE "Command failed"
 #define DATE_COMMAND_ON_SHELL "date"
+
+//----- PROTOTYPES -----
+int stringsAreEquals(char* str1, char* str2);
+void writeOrExit(char* strToWrite, char* strErrorMessage);
+void execute_fortune();
+void execute_date();
+
+//----- MAIN -----
+int main()
+{
+    char inputUserBuffer[MAX_SIZE];
+
+	writeOrExit(START_MESSAGE, WRITE_ERROR_MESSAGE);
+
+    do{
+		writeOrExit(PROMPT_MESSAGE, WRITE_ERROR_MESSAGE);
+        int bytesRead = read(STDIN_FILENO, inputUserBuffer, MAX_SIZE - 1);
+        if (bytesRead <= 0)
+        {
+            perror(READ_ERROR_MESSAGE);
+            exit(EXIT_FAILURE);
+        }
+        inputUserBuffer[bytesRead - 1] = '\0';
+        
+        // If string correspond to a command, execute the corresponding function
+        // otherwise write the COMMAND_NOT_FOUND_MESSAGE
+		if(stringsAreEquals(inputUserBuffer, FORTUNE_STRING_INPUT))
+		{
+			execute_fortune();
+		}
+		else if (stringsAreEquals(inputUserBuffer, DATE_STRING_INPUT))
+		{
+			execute_date();
+		}
+		else
+		{
+			writeOrExit(COMMAND_NOT_FOUND_MESSAGE, WRITE_ERROR_MESSAGE);
+		}
+
+    } while (!stringsAreEquals(inputUserBuffer, EXIT_STRING));
+
+	writeOrExit(EXIT_MESSAGE, WRITE_ERROR_MESSAGE);
+	
+    return 0;
+}
+
+//----- OTHER FUNCTIONS -----
 
 // Function to check if the two stings are the same
 int stringsAreEquals(char* str1, char* str2)
@@ -28,10 +77,24 @@ int stringsAreEquals(char* str1, char* str2)
     return 0;
 }
 
+// Display the message or print perror and exit
+void writeOrExit(char* strToWrite, char* strErrorMessage)
+{
+	if(write(STDOUT_FILENO, strToWrite, strlen(strToWrite))==-1)
+	{
+		perror(strErrorMessage);
+        exit(EXIT_FAILURE);
+	}
+}
+
 // Function to execute if user enters FORTUNE_STRING_INPUT
 void execute_fortune()
 {
-    write(STDOUT_FILENO, FORTUNE_MESSAGE, strlen(FORTUNE_MESSAGE));
+    if(write(STDOUT_FILENO, FORTUNE_MESSAGE, strlen(FORTUNE_MESSAGE))==-1)
+    {
+		perror(WRITE_ERROR_MESSAGE);
+        exit(EXIT_FAILURE);
+	}
 }
 
 // Function to execute if user enters DATE_STRING_INPUT
@@ -54,37 +117,4 @@ void execute_date()
         // Parent process waits for his child
         wait(NULL);
     }
-}
-
-int main()
-{
-    char inputUserBuffer[MAX_SIZE];
-
-    write(STDOUT_FILENO, START_MESSAGE, strlen(START_MESSAGE));
-
-    do{
-        write(STDOUT_FILENO, PROMPT_MESSAGE, strlen(PROMPT_MESSAGE));
-        int bytesRead = read(STDIN_FILENO, inputUserBuffer, MAX_SIZE - 1);
-        inputUserBuffer[bytesRead - 1] = '\0';
-        
-        // If string correspond to a command, execute the corresponding function
-        // otherwise write the COMMAND_NOT_FOUND_MESSAGE
-		if(stringsAreEquals(inputUserBuffer, FORTUNE_STRING_INPUT))
-		{
-			execute_fortune();
-		}
-		else if (stringsAreEquals(inputUserBuffer, DATE_STRING_INPUT))
-		{
-			execute_date();
-		}
-		else
-		{
-			write(STDOUT_FILENO, COMMAND_NOT_FOUND_MESSAGE, strlen(COMMAND_NOT_FOUND_MESSAGE));
-		}
-
-    } while (!stringsAreEquals(inputUserBuffer, EXIT_STRING));
-
-	write(STDOUT_FILENO, EXIT_MESSAGE, strlen(EXIT_MESSAGE));
-	
-    return 0;
 }
