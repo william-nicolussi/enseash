5fc91db64d6896ea57250a2b4ca152584b51b731
william-nicolussi <188550990+william-nicolussi@users.noreply.github.com>
2024-12-05 08:57:19 +0100

    Q3 ok

--- Q3/enseash_q3.c
+++ Q3/enseash_q3.c
@@ -11,21 +11,16 @@
 #define EXIT_MESSAGE "Bye bye...\n"
 #define COMMAND_NOT_FOUND_MESSAGE "Command not found.\n"
 #define READ_ERROR_MESSAGE "\nError: Unable to read input.\n"
-#define WRITE_ERROR_MESSAGE "\nError: Unable to write.\n"
+#define DISPLAY_SHELL_ERROR_MESSAGE "\nError: Unable to write.\n"
 
-#define FORTUNE_MESSAGE "Today is what happened to yesterday.\n"
-#define FORTUNE_STRING_INPUT "fortune"
-#define DATE_STRING_INPUT "date"
 #define FORK_FAILED_MESSAGE "fork failed"
 #define COMMAND_FAILED_MESSAGE "Command failed"
-#define DATE_COMMAND_ON_SHELL "date"
 
 #define CTRL_D_PRESSED_MESSAGE "As you pressed ctrl + D, the program exits"
 
 //----- PROTOTYPES -----
 int stringsAreEquals(char* str1, char* str2);
-void writeOrExit(char* strToWrite, char* strErrorMessage);
-void execute_fortune();
+void displayShell(char* strToWrite);
 void execute_fork(char* strInputShell);
 
 //----- MAIN -----
@@ -34,10 +29,9 @@ int main()
 	int programHasToStop = 0;
     char inputUserBuffer[MAX_SIZE];
 
-	writeOrExit(START_MESSAGE, WRITE_ERROR_MESSAGE);
-
+	displayShell(START_MESSAGE); 
     do{
-		writeOrExit(PROMPT_MESSAGE, WRITE_ERROR_MESSAGE);
+		displayShell(PROMPT_MESSAGE);
         int bytesRead = read(STDIN_FILENO, inputUserBuffer, MAX_SIZE - 1);
         
         // If read(...) above gets an error, stop the execution
@@ -51,8 +45,8 @@ int main()
         else if (bytesRead == 0)
         {
 			programHasToStop = 1;
-			writeOrExit(EXIT_STRING, WRITE_ERROR_MESSAGE);
-			writeOrExit("\n", WRITE_ERROR_MESSAGE);
+			displayShell(EXIT_STRING);
+			displayShell("\n");
 			continue;
 		}
 		else
@@ -60,23 +54,16 @@ int main()
 			inputUserBuffer[bytesRead - 1] = '\0';
 		}
         
-        // If string correspond to a command, execute the corresponding function
-        // otherwise write the COMMAND_NOT_FOUND_MESSAGE
-		if(stringsAreEquals(inputUserBuffer, FORTUNE_STRING_INPUT))
-		{
-			execute_fortune();
-		}
-		else if (stringsAreEquals(inputUserBuffer, EXIT_STRING))
+		if (stringsAreEquals(inputUserBuffer, EXIT_STRING))
 		{
 			programHasToStop = 1;
 		}
-		else
-		{
-			execute_fork(inputUserBuffer);
-		}
+
+		execute_fork(inputUserBuffer);
+		
     } while (!programHasToStop);
 
-	writeOrExit(EXIT_MESSAGE, WRITE_ERROR_MESSAGE);
+	displayShell(EXIT_MESSAGE);
 	
     return 0;
 }
@@ -94,21 +81,11 @@ int stringsAreEquals(char* str1, char* str2)
 }
 
 // Display the message or print perror and exit
-void writeOrExit(char* strToWrite, char* strErrorMessage)
+void displayShell(char* strToWrite)
 {
 	if(write(STDOUT_FILENO, strToWrite, strlen(strToWrite))==-1)
 	{
-		perror(strErrorMessage);
-        exit(EXIT_FAILURE);
-	}
-}
-
-// Function to execute if user enters FORTUNE_STRING_INPUT
-void execute_fortune()
-{
-    if(write(STDOUT_FILENO, FORTUNE_MESSAGE, strlen(FORTUNE_MESSAGE))==-1)
-    {
-		perror(WRITE_ERROR_MESSAGE);
+		perror(DISPLAY_SHELL_ERROR_MESSAGE);
         exit(EXIT_FAILURE);
 	}
 }
